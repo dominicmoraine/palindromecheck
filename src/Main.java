c1abb2364ae4cdc7d625936e5815883e74fd956e
dominicmoraine <discordpopcornuser@gmail.com>
2026-09-21 01:18:51 -0500

    script update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Palindrome Checker");
+        System.out.println("------------------");
+        System.out.print("\nEnter a word or phrase: ");
+
+        String originalInput = scanner.nextLine();
+        String lowercaseInput = originalInput.toLowerCase();
+        StringBuilder cleanedText = new StringBuilder();
+
+        for (int i = 0; i < lowercaseInput.length(); i++) {
+            char character = lowercaseInput.charAt(i);
+
+            if (Character.isLetterOrDigit(character)) {
+                cleanedText.append(character);
+            }
+        }
+
+        StringBuilder reversedText = new StringBuilder(cleanedText);
+        reversedText.reverse();
+
+        boolean isPalindrome = cleanedText.toString().equals(reversedText.toString());
+
+        System.out.println("\nOriginal input: " + originalInput);
+        System.out.println("Cleaned text: " + cleanedText);
+        System.out.println("Reversed text: " + reversedText);
+
+        if (isPalindrome) {
+            System.out.println("\nResult: This is a palindrome.");
+        } else {
+            System.out.println("\nResult: This is not a palindrome.");
+        }
+
+        scanner.close();
+    }
 }
